67d3547b8bfc6e23e626ca0b424369f8c6e1a457
Matheus R Pedreira <matrpedreira@gmail.com>
2016-07-10 23:05:48 -0300

    Primeiro commit

---
+++ App cidadão/Cade,Sus/app/src/main/java/cadesus/co/cadesus/LoginActivity.java
@@ -0,0 +1,17 @@
+package cadesus.co.cadesus;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Fraps on 10/07/2016.
+ */
+public class LoginActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+}
